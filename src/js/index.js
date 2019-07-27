cd0d13a46df99ec0deeedf0d166f5a656b5416bf
elaine <kim85326@gmail.com>
2019-07-27 19:51:14 +0800

    新增簡易版拖曳功能

--- src/js/index.js
+++ src/js/index.js
@@ -3,50 +3,33 @@ const homeCellSlots = [[], [], [], []];
 const otherCellSlots = [[], [], [], [], [], [], [], []];
 
 function initGame() {
-    let card = getInitialCards();
-    card = shuffle(card);
-
-    for (let i = 0; i < otherCellSlots.length; i++) {
-        let slotCapacity = 7;
-        if (i > 3) {
-            slotCapacity = 6;
-        }
-
-        for (let j = 0; j < slotCapacity; j++) {
-            otherCellSlots[i].push(card[i * slotCapacity + j]);
-        }
-    }
+    let cards = getInitialCards();
+    cards = shuffle(cards);
+    initOtherCellSlots(cards);
 }
 
 function getInitialCards() {
     const cards = [];
 
     for (let i = 1; i <= 52; i++) {
-        cards.push(i);
+        const card = {
+            point: i,
+            color: convertCardToColor(i),
+            suit: convertCardToSuit(i),
+            number: convertCardToNumber(i),
+            englishNumber: convertCardToEnglishNumber(i),
+            draggable: false,
+            droppable: false,
+            slotNumber: 0,
+        };
+        cards.push(card);
     }
 
     return cards;
 }
 
-function shuffle(cards) {
-    // 複製一份牌組陣列
-    let shuffledCards = { ...cards };
-
-    for (let i = 0; i < 52; i++) {
-        // 隨機取一個位置
-        let j = Math.floor(Math.random() * 52);
-
-        // 交換兩張牌
-        let temp = shuffledCards[j];
-        shuffledCards[j] = shuffledCards[i];
-        shuffledCards[i] = temp;
-    }
-
-    return shuffledCards;
-}
-
-function convertCardToColor(card) {
-    const suit = Math.ceil(card / 13);
+function convertCardToColor(point) {
+    const suit = Math.ceil(point / 13);
 
     switch (suit) {
         case 1: //梅花
@@ -58,8 +41,8 @@ function convertCardToColor(card) {
     }
 }
 
-function convertCardToSuit(card) {
-    const suit = Math.ceil(card / 13);
+function convertCardToSuit(point) {
+    const suit = Math.ceil(point / 13);
 
     switch (suit) {
         case 1:
@@ -73,12 +56,18 @@ function convertCardToSuit(card) {
     }
 }
 
-function convertCardToNumber(card) {
-    return card % 13;
+function convertCardToNumber(point) {
+    const number = point % 13;
+
+    if (number === 0) {
+        return 13;
+    }
+
+    return number;
 }
 
-function convertCardToEnglishNumber(card) {
-    const number = card % 13;
+function convertCardToEnglishNumber(point) {
+    const number = point % 13;
 
     switch (number) {
         case 1:
@@ -87,10 +76,48 @@ function convertCardToEnglishNumber(card) {
             return "J";
         case 12:
             return "Q";
-        case 12:
+        case 0:
             return "K";
         default:
             return number;
+    }
+}
+
+function shuffle(cards) {
+    let shuffledCards = JSON.parse(JSON.stringify(cards));
+
+    for (let i = 0; i < 52; i++) {
+        let j = Math.floor(Math.random() * 52);
+
+        let temp = shuffledCards[j];
+        shuffledCards[j] = shuffledCards[i];
+        shuffledCards[i] = temp;
+    }
+
+    return shuffledCards;
+}
+
+function initOtherCellSlots(cards) {
+    for (let i = 0; i < otherCellSlots.length; i++) {
+        let slotCapacity;
+
+        if (i <= 3) {
+            slotCapacity = 7;
+
+            for (let j = 0; j < slotCapacity; j++) {
+                const card = cards[i * slotCapacity + j];
+                card.slotNumber = i;
+                otherCellSlots[i].push(card);
+            }
+        } else {
+            slotCapacity = 6;
+
+            for (let j = 0; j < slotCapacity; j++) {
+                const card = cards[28 + (i - 4) * slotCapacity + j];
+                card.slotNumber = i;
+                otherCellSlots[i].push(card);
+            }
+        }
     }
 }
 
@@ -108,6 +135,7 @@ function refreshGameView() {
     refreshCellSlotsView(otherCellSlotsDom, otherCellSlots);
     refreshCellSlotsView(freeCellSlotsDom, freeCellSlots);
     refreshCellSlotsView(homeCellSlotsDom, homeCellSlots);
+    setCardDomsDraggable();
 }
 
 function refreshCellSlotsView(slotsDom, slots) {
@@ -121,19 +149,20 @@ function refreshCellSlotsView(slotsDom, slots) {
 }
 
 function getCardDom(card) {
-    const color = convertCardToColor(card);
-    const suit = convertCardToSuit(card);
-    const englishNumber = convertCardToEnglishNumber(card);
-    let imgFileName = `${suit}`;
-    if (card % 13 > 10) {
-        imgFileName = `${englishNumber}-${color}`;
+    let imgFileName = `${card.suit}`;
+    if (card.point % 13 > 10) {
+        imgFileName = `${card.englishNumber}-${card.color}`;
     }
 
-    return `<div class="card ${color}" id="card-${card}">
+    return `<div class="card ${card.color}" data-point="${
+        card.point
+    }" data-draggable=${card.draggable} data-droppable=${
+        card.droppable
+    } data-slot-number=${card.slotNumber}>
                 <div class="card-header">
-                    <div class="card-number">${englishNumber}</div>
+                    <div class="card-number">${card.englishNumber}</div>
                     <img
-                        src="./images/${suit}_s.svg"
+                        src="./images/${card.suit}_s.svg"
                         alt=""
                         class="card-suit"
                     />
@@ -155,51 +184,151 @@ function main() {
 
 main();
 
-// let isDragging = false;
-// let draggingDom;
-// const initDraggingDomPosition = {
-//     originX: 0,
-//     originY: 0,
-//     offsetX: 0,
-//     offsetY: 0,
-// };
-// let draggingDomPosition = { ...initDraggingDomPosition };
+function setCardDomsDraggable() {
+    const slotDoms = document.querySelectorAll(".card-slot");
+    slotDoms.forEach((slotDom) => {
+        if (slotDom.lastChild) {
+            slotDom.lastChild.dataset.draggable = true;
+        }
+    });
+}
 
-// const game = document.querySelector("#game");
-// game.addEventListener("mousedown", mousedown);
+function setCardDomsDroppable() {
+    const slotDoms = document.querySelectorAll(".card-slot");
+    slotDoms.forEach((slotDom) => {
+        if (slotDom.lastChild) {
+            slotDom.lastChild.dataset.droppable = true;
+        }
+    });
+}
 
-// function mousedown(event) {
-//     if (!event.target.classList.contains("card")) {
-//         return;
-//     }
-//     console.log("mousedown");
-//     isDragging = true;
-//     draggingDom = event.target;
-//     draggingDom.style.zIndex = 999;
-//     draggingDomPosition.originX = event.pageX;
-//     draggingDomPosition.originY = event.pageY;
-//     document.addEventListener("mousemove", mousemove);
-//     document.addEventListener("mouseup", mouseup);
-// }
+let isDragging = false;
+let draggingDom;
+const initDraggingDomPosition = {
+    originX: 0,
+    originY: 0,
+    offsetX: 0,
+    offsetY: 0,
+};
+let draggingDomPosition = { ...initDraggingDomPosition };
 
-// function mousemove(event) {
-//     if (!isDragging) {
-//         return;
-//     }
-//     console.log("mousemove");
-//     const dx = event.pageX - draggingDomPosition.originX;
-//     const dy = event.pageY - draggingDomPosition.originY;
-//     draggingDom.style.transform = `translate(${draggingDomPosition.offsetX +
-//         dx}px, ${draggingDomPosition.offsetY + dy}px)`;
-// }
+const game = document.querySelector("#game");
+game.addEventListener("mousedown", mousedown);
 
-// function mouseup(event) {
-//     if (!isDragging) {
-//         return;
-//     }
-//     console.log("mouseup");
-//     draggingDomPosition = initDraggingDomPosition;
-//     document.removeEventListener("mousemove", mousemove);
-//     document.removeEventListener("mouseup", mouseup);
-//     isDragging = false;
-// }
+function mousedown(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains("card")) {
+        return;
+    }
+    if (!event.target.dataset.draggble === false) {
+        return;
+    }
+    setCardDomsDroppable();
+    isDragging = true;
+    draggingDom = event.target;
+    draggingDom.style.zIndex = 999;
+    draggingDomPosition.originX = event.pageX;
+    draggingDomPosition.originY = event.pageY;
+    document.addEventListener("mousemove", mousemove);
+    document.addEventListener("mouseup", mouseup);
+}
+
+function mousemove(event) {
+    event.preventDefault();
+    if (!isDragging) {
+        return;
+    }
+    const dx = event.pageX - draggingDomPosition.originX;
+    const dy = event.pageY - draggingDomPosition.originY;
+    draggingDom.style.transform = `translate(${draggingDomPosition.offsetX +
+        dx}px, ${draggingDomPosition.offsetY + dy}px)`;
+}
+
+function mouseup(event) {
+    event.preventDefault();
+    if (!isDragging) {
+        return;
+    }
+    const droppableDoms = document.querySelectorAll(
+        ".card[data-droppable=true]"
+    );
+
+    let droppingDom = null;
+    const draggingDomClientPosition = draggingDom.getBoundingClientRect();
+
+    for (let i = 0; i < droppableDoms.length; i++) {
+        const droppableDom = droppableDoms[i];
+
+        // 自己不算
+        if (droppableDom === draggingDom) {
+            continue;
+        }
+
+        const droppableDomClientPosition = droppableDom.getBoundingClientRect();
+
+        // dragging 任何一個角在 droppable 的範圍內
+        // 左上角
+        if (
+            droppableDomClientPosition.left <= draggingDomClientPosition.left &&
+            draggingDomClientPosition.left <=
+                droppableDomClientPosition.right &&
+            droppableDomClientPosition.top <= draggingDomClientPosition.top &&
+            draggingDomClientPosition.top <= droppableDomClientPosition.bottom
+        ) {
+            droppingDom = droppableDom;
+            break;
+        }
+        // 右上角
+        if (
+            droppableDomClientPosition.left <=
+                draggingDomClientPosition.right &&
+            draggingDomClientPosition.right <=
+                droppableDomClientPosition.right &&
+            droppableDomClientPosition.top <= draggingDomClientPosition.top &&
+            draggingDomClientPosition.top <= droppableDomClientPosition.bottom
+        ) {
+            droppingDom = droppableDom;
+            break;
+        }
+        // 左下角
+        if (
+            droppableDomClientPosition.left <= draggingDomClientPosition.left &&
+            draggingDomClientPosition.left <=
+                droppableDomClientPosition.right &&
+            droppableDomClientPosition.top <=
+                draggingDomClientPosition.bottom &&
+            draggingDomClientPosition.bottom <=
+                droppableDomClientPosition.bottom
+        ) {
+            droppingDom = droppableDom;
+            break;
+        }
+        // 右下角
+        if (
+            droppableDomClientPosition.left <=
+                draggingDomClientPosition.right &&
+            draggingDomClientPosition.right <=
+                droppableDomClientPosition.right &&
+            droppableDomClientPosition.top <=
+                draggingDomClientPosition.bottom &&
+            draggingDomClientPosition.bottom <=
+                droppableDomClientPosition.bottom
+        ) {
+            droppingDom = droppableDom;
+            break;
+        }
+    }
+
+    if (droppingDom !== null) {
+        const card = otherCellSlots[draggingDom.dataset.slotNumber].pop();
+        card.slotNumber = droppingDom.dataset.slotNumber;
+        otherCellSlots[droppingDom.dataset.slotNumber].push(card);
+    }
+
+    refreshGameView();
+    draggingDomPosition = initDraggingDomPosition;
+    document.removeEventListener("mousemove", mousemove);
+    document.removeEventListener("mouseup", mouseup);
+    draggingDom.style.zIndex = "";
+    isDragging = false;
+}
